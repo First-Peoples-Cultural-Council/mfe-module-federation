e64543c62245f33e30301e038ec93752f9ea5973
Chad Oakenfold <web@oakenfold.ca>
2021-01-06 12:05:27 -0800

    Rename app-a > app-v1

--- apps/app-v1/src/App.jsx
+++ apps/app-v1/src/App.jsx
@@ -4,18 +4,18 @@ import Footer from './Footer';
 import Sidebar from './Sidebar';
 function App({children}) {
   return (
-    <div className="AppA">
-      <Header className="AppA__header" />
-      <main role="main" className="AppA__main">
-        <Sidebar className="AppA__sidebar"/>
-        <div className="AppA__mainContent">
+    <div className="AppV1">
+      <Header className="AppV1__header" />
+      <main role="main" className="AppV1__main">
+        <Sidebar className="AppV1__sidebar"/>
+        <div className="AppV1__mainContent">
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
           <p>Quis sit dicta distinctio tempora, voluptatibus illum tenetur a veritatis.</p>
           <p>Inventore quisquam earum incidunt sequi maiores voluptates nemo tempore.</p>
           <p>Perspiciatis cumque accusamus!</p>
         </div>
       </main>
-      <Footer className="AppA__footer" />
+      <Footer className="AppV1__footer" />
     </div>
   );
 }
