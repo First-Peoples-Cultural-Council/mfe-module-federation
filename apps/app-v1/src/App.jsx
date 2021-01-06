3c144fd1e3142ae5d2878d1fb2cb136b88fad99b
Chad Oakenfold <web@oakenfold.ca>
2021-01-06 13:28:40 -0800

    Fallback proof of concept for downed remote

--- apps/app-v1/src/App.jsx
+++ apps/app-v1/src/App.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import Header from 'app_v2/Header';
+import React, {Suspense} from 'react';
+// import Header from 'app_v2/Header';
 import Footer from './Footer';
+import ErrorBoundary from './ErrorBoundary';
 import Sidebar from './Sidebar';
+
+const loadScope = (url, scope) => {
+  const element = document.createElement('script');
+  const promise = new Promise((resolve, reject) => {
+    element.src = url
+    element.type = 'text/javascript'
+    element.async = true
+    element.onload = () => resolve(window[scope])
+    element.onerror = reject
+  })
+  document.head.appendChild(element)
+  promise.finally(() => document.head.removeChild(element))
+  return promise
+}
+
+const loadModule = async (url, scope, module) => {
+  try {
+    const container = await loadScope(url, scope)
+    await __webpack_init_sharing__('default')
+    await container.init(__webpack_share_scopes__.default)
+    const factory = await container.get(module)
+    return factory()
+  } catch (error) {
+    console.error('Error loading module:', error)
+    throw error
+  }
+}
+
+const Header = React.lazy(() => loadModule(
+  'http://localhost:8001/remoteEntry.js',
+  'app_v2',
+  './Header'
+))
+const HeaderBackup = React.lazy(() => import('./fallbacks/Header'))
 function App({children}) {
   return (
     <div className="AppV1">
-      <Header className="AppV1__header" />
+      <Suspense fallback={<div>Loading...</div>}><ErrorBoundary fallback={<HeaderBackup className="AppV1__header" />}><Header className="AppV1__header" /></ErrorBoundary></Suspense>
       <main role="main" className="AppV1__main">
         <Sidebar className="AppV1__sidebar"/>
         <div className="AppV1__mainContent">
